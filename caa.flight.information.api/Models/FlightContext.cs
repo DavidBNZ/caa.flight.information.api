688b9e004aa20c3a54a2ab74618f119d8c28045a
David <341625+dpb06@users.noreply.github.com>
2024-08-22 11:47:57 +1200

    Add Base FlightsController

---
+++ caa.flight.information.api/Models/FlightContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace caa.flight.information.api.Models
+{
+    public class FlightContext : DbContext
+    {
+        public FlightContext(DbContextOptions<FlightContext> options) : base(options) {}
+
+        public DbSet<Flight> Flights { get; set; } = null!;
+    }
+}
--- caa.flight.information.api/Program.cs
+++ caa.flight.information.api/Program.cs
@@ -1,8 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using caa.flight.information.api.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<FlightContext>(options => options.UseInMemoryDatabase("Flights"));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
